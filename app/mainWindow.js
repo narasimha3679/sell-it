ce6f87280364fd17738a888bf1fa40d916397a61
narasimha3679 <narasimha.sinivarapu@gmail.com>
2019-02-09 10:05:18 +0530

    added drop autocompleate in search

--- app/mainWindow.js
+++ app/mainWindow.js
@@ -1,39 +1,58 @@
+const electron = require('electron');
+const ipc = electron.ipcRenderer;
+var searchresults;
+var selectedProductId;
+var itemsList = [];
+$(document).ready(function () {
+    var lengh;
 
-$(document).ready(function(){
-
-    var formData = {
-        firstname : "nara",
-        lastname :  "simah"
-    };
-    $.ajax({
-
-        type : "POST",
-        contentType : "application/json",
-        url: "http://localhost:3000/",
-        data : JSON.stringify(formData),
-        dataType : 'json',
-
-
-        success: function (response) {
-
-            if(response.success){
-
-                console.log("true");
-
-            }else{
-
-                console.log("false");
-
-            }
-
-        },
-
-        error: function (error) {
-
-            console.log("error");
-
+    // send data to main when typed more than 2 letters
+    $('#search-name').on('keyup', function () {
+        lengh = $('#search-name').val().length;
+        if (lengh > 2) {
+            ipc.send("search-query", $('#search-name').val());
         }
+    });
+
+    // taking search query results from main
+    ipc.on("search-results", function (event, arg) {
+
+        searchresults = arg;
+        console.table(arg);
 
     });
 
+    // Below is the name of the textfield that will be autocomplete
+    $('#search-name').autocomplete({
+        // This shows the min length of charcters that must be typed before the autocomplete looks for a match.
+        minLength: 2,
+        source: function (request, response) {
+            response($.map(searchresults, function (value, key) {
+                return {
+                    label: value.productName,
+                    value: value.productCode
+                }
+            }));
+
+        },
+          focus: function(event, ui) {
+              console.log(ui.item.label);
+              $('#search-name').text(ui.item.label);
+              return false;
+          },
+        // Once a value in the drop down list is selected, do the following:
+        select: function (event, ui) {
+
+            selectedProductId = ui.item.value;
+
+            console.log(selectedProductId);
+            itemsList.push(selectedProductId);
+            $('#search-name').val('');
+            /*  // place the person.given_name value into the textfield called 'select_origin'...
+              $('#search-name').val(ui.item.label);
+              // and place the person.id into the hidden textfield called 'link_origin_id'.
+              $('#search-id').val(ui.item.value);*/
+            return false;
+        }
+    });
 });
--- main.js
+++ main.js
@@ -1,39 +1,68 @@
 const electron = require('electron');
 const url = require('url');
 const path = require('path');
-
 const {app, BrowserWindow, Menu} = electron;
+const sqlite3 = require('sqlite3').verbose();
+const ipc = electron.ipcMain;
+let db = new sqlite3.Database('./database/database.sqlite3');
+require('electron-reload')(__dirname);
 
 let mainWindow;
 
-//listen when app is ready
-app.on('ready', function () {
+function createWindow() {
+
     //creating new window
-   mainWindow = new BrowserWindow({});
-   //load html into window
-    mainWindow.loadURL(url.format({
-        pathname: path.join(__dirname, '/app/mainWindow.html'),
-        protocol: 'file:',
-        slashes: true
-    }));
-    /*const mainMenu = Menu.buildFromTemplate(mainMenuTemplate);
-    Menu.setApplicationMenu(mainMenu);*/
+    mainWindow = new BrowserWindow({});
+    //load html into window
+    mainWindow.loadFile('./app/mainWindow.html');
+
+    /*ipc start*/
+    ipc.on('search-query', function (event, arg) {
+        db.serialize(function () {
+
+            db.all("SELECT productName, productCode, productPrice, productDiscount FROM products WHERE productName LIKE '%"+arg+"%' LIMIT 5", function (err, row) {
+                console.log(row);
+                event.sender.send("search-results",row);
+            })
+
+
+        })
+
+    });
+    /*ipc end*/
+
+    /*
+        // Open the DevTools.
+        mainWindow.webContents.openDevTools()
+    */
+
+    // Emitted when the window is closed.
+    mainWindow.on('closed', () => {
+        // Dereference the window object, usually you would store windows
+        // in an array if your app supports multi windows, this is the time
+        // when you should delete the corresponding element.
+        mainWindow = null
+    })
+}
+
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+app.on('ready', createWindow);
+
+// Quit when all windows are closed.
+app.on('window-all-closed', () => {
+    // On macOS it is common for applications and their menu bar
+    // to stay active until the user quits explicitly with Cmd + Q
+    if (process.platform !== 'darwin') {
+        app.quit()
+    }
 });
 
-/*
-const mainMenuTemplate = [
-    {
-        label: 'Help',
-        submenu : [
-            {
-               label: 'About'
-            },
-            {
-                label: 'quit',
-                click(){
-                    app.quit();
-                }
-            }
-        ]
+app.on('activate', () => {
+    // On macOS it's common to re-create a window in the app when the
+    // dock icon is clicked and there are no other windows open.
+    if (mainWindow === null) {
+        createWindow()
     }
-]*/
+});
+
